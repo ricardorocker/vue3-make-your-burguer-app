307c98010cf3ea219c3fa3b80fb82df4254cbc05
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-12-06 00:34:01 -0300

    Banner do app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="main-container">
-    <h1>HOME</h1>
+  <div>
+    <banner />
+    <div class="main-container">
+      <h1>HOME</h1>
+    </div>
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue";
+
+export default {
+  components: { Banner },
+  name: "HomeView",
+};
 </script>
