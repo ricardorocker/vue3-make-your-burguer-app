d9b530e51434ee201555649b47c8ede4db9e1c93
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-12-06 02:03:56 -0300

    Resgatando dados do banco e inserindo no formulário

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -17,50 +17,32 @@
           <label for="bread">Escolha o pão:</label>
           <select name="bread" id="bread" v-model="bread">
             <option value="">Selecione o tipo do pão</option>
-            <option value="frances">Francês</option>
+            <option v-for="bread in breads" :key="bread.id" :value="bread.tipo">{{bread.tipo}}</option>
           </select>
         </div>
         <div class="input-container">
           <label for="meat">Escolha a carne do seu Burger:</label>
           <select name="meat" id="meat" v-model="meat">
             <option value="">Selecione o tipo da carne</option>
-            <option value="frances">Maminha</option>
+            <option v-for="meat in meats" :key="meat.id" :value="meat.tipo">{{meat.tipo}}</option>
           </select>
         </div>
         <div id="opcionais-container" class="input-container">
           <label id="opcionais-title" for="additionals"
             >Selecione os opcionais:</label
           >
-          <div class="checkbox-container">
+          <div class="checkbox-container" v-for="additional in additionalsData" :key="additional.id">
             <input
               type="checkbox"
               name="additionals"
-              value="additionals"
+              :value="additional.tipo"
               v-model="additionals"
             />
-            <span>Salame</span>
-          </div>
-          <div class="checkbox-container">
-            <input
-              type="checkbox"
-              name="additionals"
-              value="additionals"
-              v-model="additionals"
-            />
-            <span>Salame</span>
-          </div>
-          <div class="checkbox-container">
-            <input
-              type="checkbox"
-              name="additionals"
-              value="additionals"
-              v-model="additionals"
-            />
-            <span>Salame</span>
+            <span>{{additional.tipo}}</span>
           </div>
         </div>
         <div class="input-container">
-            <input type="submit" class="submit-btn" value="Criar meu Burger!">
+          <input type="submit" class="submit-btn" value="Criar meu Burger!" />
         </div>
       </form>
     </div>
@@ -70,6 +52,34 @@
 <script>
 export default {
   name: "BurgerForm",
+  data() {
+    return {
+        breads: null,
+        meats: null,
+        additionalsData: null,
+        name: null,
+        bread: null,
+        meat: null,
+        additionals: [],
+        status: "Solicitado",
+        msg: null
+    }
+  },
+  methods: {
+    async getIngredients() {
+        const req = await fetch("http://localhost:3000/ingredientes");
+        const data = await req.json();
+
+        console.log(data);
+
+        this.breads = data.paes;
+        this.meats = data.carnes;
+        this.additionalsData = data.opcionais;
+    }
+  },
+  mounted() {
+    this.getIngredients();
+  }
 };
 </script>
 
@@ -117,29 +127,28 @@ select {
 
 .checkbox-container span,
 .checkbox-container input {
-    width: auto;
+  width: auto;
 }
 
 .checkbox-container span {
-    margin-left: 6px;
-    font-weight: bold;
+  margin-left: 6px;
+  font-weight: bold;
 }
 
 .submit-btn {
-    background-color: #222;
-    color: #fcba03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
 }
 
 .submit-btn:hover {
-    background-color: transparent;
-    color: #222;
+  background-color: transparent;
+  color: #222;
 }
-
 </style>
